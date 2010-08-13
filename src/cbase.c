86f59edaa4175e08a1a5753f2ddfe8c7f3e68361
AZ Huang <aitjcize@gmail.com>
2010-08-13 17:42:28 +0800

    Increase version number to 0.3

--- src/cbase.c
+++ src/cbase.c
@@ -1,12 +1,13 @@
-/* cbase.c
- * - change base of numbers
-
- * Copyright (C) 2009 -  Aitjcize <aitjcize@gmail.com>
+/**
+ * cbase.c - A simple program for base convertion between numbers.
+ *
+ * Copyright (C) 2010 -  Wei-Ning Huang (AZ) <aitjcize@gmail.com>
  * All Rights reserved.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License version 2 as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +15,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <getopt.h>
@@ -31,7 +33,7 @@
 #define L_FATAL   0x04
 
 const char* program_name = "cbase";
-const char* program_version = "0.2";
+const char* program_version = "0.3";
 
 static struct option longopts[] = {
   { "input-base",                required_argument, NULL, 'i' },
