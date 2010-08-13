417208bd07fa6b7f08b9fd44db521d0dc8182ee5
AZ Huang <aitjcize@gmail.com>
2010-08-13 19:20:09 +0800

    Add sdist, clean to build script

--- dev/build-pkg.sh
+++ dev/build-pkg.sh
@@ -2,27 +2,38 @@
 
 program_name=cbase
 
+if [ "$1" == "clean" ]; then
+  rm -rf ${program_name}*
+  exit 0
+fi
+
 echo 'Version: '
 read ver
 
 ./chver.sh $ver
 cd ../../
 cp -r $program_name $program_name-$ver
+rm -rf $program_name-$ver/dev
 mv $program_name-$ver $program_name/dev
 cd $program_name/dev/
-tar -zcf ${program_name}_$ver.orig.tar.gz $program_name-$ver
+
+if [ "$1" == "sdist" ]; then
+  tar -zcf ${program_name}-$ver.tar.gz $program_name-$ver
+  exit 0
+else
+  tar -zcf ${program_name}_$ver.orig.tar.gz $program_name-$ver
+fi
 
 cd $program_name-$ver
 dh_make -s -b -p $program_name
 
-echo `pwd`
-cp dev/rules dev/control dev/manpages debian
+cp ../rules ../control ../manpages debian
 cd debian
 rm *.ex *.EX README.*
 dch -e
 cd ..
 
-if [ "$1" == "deb" ]; then
+if [ "$1" == "deb" ] || [ -z "$1" ]; then
   dpkg-buildpackage -rfakeroot
 elif [ "$1" == "ppa" ]; then
   debuild -S
