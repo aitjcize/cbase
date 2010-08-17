a86969853224ab372f2b06c2b93b38831d03e1f1
AZ Huang <aitjcize@gmail.com>
2010-08-17 12:25:15 +0800

    change build system to autotools

--- src/cbase.c
+++ src/cbase.c
@@ -19,6 +19,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <getopt.h>
 #include <math.h>
 #include <stdarg.h>
